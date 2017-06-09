c03bb850f0e966d6b0c02fe42e1c09ad8bb5a3f6
AkiSato <sato.geijo.13@gmail.com>
2017-06-09 12:50:08 +0900

    不要ファイル削除

--- tsx/App.js
+++
@@ -1,3 +0,0 @@
-import * as ReactDOM from "react-dom";
-import MainView from "./MainView";
-ReactDOM.render(React.createElement(MainView, null), document.getElementById('react'));
--- tsx/MainView.js
+++
@@ -1,3 +0,0 @@
-import * as React from "react";
-export class MainView extends React.Component {
-}
